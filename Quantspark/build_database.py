a79fab77ec4b955083dcf5999644a6952deed402
Alastair Foster <alastairh.foster@gmail.com>
2023-06-19 10:54:39 +0100

    adding initial explore notebook and classes for build database

--- Quantspark/build_database.py
+++ Quantspark/build_database.py
@@ -1,1 +1,44 @@
 # Use this script to write the python needed to complete this task
+import requests as re
+import pandas as pd
+
+
+
+
+
+class SQLClient():
+    def __init__(
+            self,
+            server=None,
+            database=None
+    ):
+        self.server = server
+        self.database = database
+
+    def build_tables(
+            script
+    ):
+        pass
+
+    def send_query(
+            query
+    ):
+        pass
+
+class APIClient():
+    def __init__(
+            self,
+            url=None,
+            request=None
+    ):
+        self.url = url
+        self.request = request
+    
+    def send_request(
+            url,
+            request
+    ):
+        pass
+
+
+
