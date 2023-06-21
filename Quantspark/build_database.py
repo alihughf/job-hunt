00f6a62adabf16b37edc086ac92e57a919cfda69
Alastair Foster <alastairh.foster@gmail.com>
2023-06-21 14:52:52 +0100

    filling out the dimension table build

--- Quantspark/build_database.py
+++ Quantspark/build_database.py
@@ -2,9 +2,7 @@
 from apiclient import APIClient
 import pandas as pd
 import sqlite3
-
-
-
+import time
 
 class SQLClient():
     def __init__(
@@ -55,8 +53,13 @@ class SQLClient():
 class CocktailClient(APIClient):
     
     def list_all_cocktails(self, letter):
-        url = f"http://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+        url = f"http://www.thecocktaildb.com/api/json/v1/1/search.php?"
         params = {"f": letter}
+        return(self.get(url,params))
+    
+    def get_info_for_cocktail_name(self, cocktail_name):
+        url = "http://www.thecocktaildb.com/api/json/v1/1/search.php?"
+        params = {"s": cocktail_name}
         return(self.get(url,params))
 
 def create_tables(db_client: SQLClient):
@@ -105,11 +108,51 @@ def create_tables(db_client: SQLClient):
     db_client.build_tables(sales_table)
     db_client.build_tables(stock_table)
 
-def create_data_from_csv(
-
-):
-    pass
+def return_glass(cocktail_client, drink_name):
+    res = cocktail_client.get_info_for_cocktail_name(drink_name).json()
+    time.sleep(5)
+    return (res['drinks'][0]['strGlass'])
 
 def main():
     cocktail_client = CocktailClient()
     db_client = SQLClient(database="bars.db")
+    create_tables(db_client=db_client)
+
+    bar_data = pd.read_csv("data/bar_data.csv")
+    bar_data['glass_type'] = bar_data['glass_type'].replace('coper mug','copper mug')
+    london = pd.read_csv("data/london_transactions.csv.gz", compression="gzip", sep=r"\t", names=['datetime','drink','price'], index_col=0)
+    budapest = pd.read_csv("data/budapest.csv.gz", compression="gzip",names=['datetime','drink','price'], index_col=0, header=0)
+    ny = pd.read_csv("data/ny.csv.gz", compression="gzip",names=['datetime','drink','price'], index_col=0, header=0)
+
+    london['datetime'] = london['datetime'].apply(pd.to_datetime)
+    budapest['datetime'] = budapest['datetime'].apply(pd.to_datetime)
+    ny['datetime'] = ny['datetime'].apply(pd.to_datetime)
+
+    #creating drinks table
+    all_drinks = pd.DataFrame(set(london['drink'].unique()) | set(budapest['drink'].unique()) | set(ny['drink'].unique()), columns=["DrinkName"])
+    all_drinks['DrinkID'] = all_drinks.index
+    all_drinks["Glass"] = all_drinks.apply(lambda row: return_glass(cocktail_client, row['DrinkName']), axis=1)
+
+    #Creating glasses dim table
+    glasses = bar_data.groupby("glass_type").max().reset_index()
+    glasses['ID'] = glasses.index
+    glasses.rename(columns = {'ID': 'GlassID', 'glass_type': 'GlassName'}, inplace=True)
+    db_client.insert_df(glasses['GlassID','GlassName'], "glasses_dim")
+
+    #creating bar dim table
+    bars = pd.DataFrame({"BarID": [1,2,3],"BarName": ["london","budapest","new york"]})
+    db_client.insert_df(bars, "bar_dim")
+
+    #creating price table
+    london_price = london[['drink','price']].groupby("drink").first()
+    london_price['BarID'] = 1
+    london_price.rename()
+
+    budapest_price = budapest[['drink','price']].groupby("drink").first()
+    budapest_price['BarID'] = 2
+
+    ny_price = ny[['drink','price']].groupby("drink").first()
+    ny_price['BarID'] = 3
+
+
+
