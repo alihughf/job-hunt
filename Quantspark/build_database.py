bff145bf5302bcce7df3c046436c0aa22804b4ef
Alastair Foster <alastairh.foster@gmail.com>
2023-06-21 09:38:32 +0100

    building out my clients

--- Quantspark/build_database.py
+++ Quantspark/build_database.py
@@ -1,7 +1,7 @@
 # Use this script to write the python needed to complete this task
-import requests as re
+from apiclient import APIClient
 import pandas as pd
-
+import sqlite3
 
 
 
@@ -9,36 +9,56 @@ import pandas as pd
 class SQLClient():
     def __init__(
             self,
-            server=None,
             database=None
     ):
-        self.server = server
         self.database = database
 
     def build_tables(
+            self,
             script
     ):
-        pass
+        con = sqlite3.connect(self.database)
+        cur = con.cursor()
+        cur.execute(script)
+        cur.close()
+        con.close()
+        return(f"Script ran on bars.db: {script}")
+    
+    def insert_df(
+            self,
+            df,
+            table_name
+    ):
+        con = sqlite3.connect(self.database)
+        df.to_sql(table_name, con, if_exists='append')
+        return(None)
 
     def send_query(
+            self,
             query
     ):
-        pass
-
-class APIClient():
-    def __init__(
-            self,
-            url=None,
-            request=None
-    ):
-        self.url = url
-        self.request = request
+        con = sqlite3.connect(self.database)
+        cur = con.cursor()
+        res = cur.execute(query)
+        cur.close()
+        con.close()
+        return(res.fetchall())
     
-    def send_request(
-            url,
-            request
+    def query_to_df(
+            self,
+            query
     ):
-        pass
+        con = sqlite3.connect(self.database)
+        df = pd.read_sql(query, con)
+        return(df)
+
+class CocktailClient(APIClient):
+    
+    def list_all_cocktails(self, letter):
+        url = f"http://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+        params = {"f": letter}
+        return(self.get(url,params))
+
 
 
 
